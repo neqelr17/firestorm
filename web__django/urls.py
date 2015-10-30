effe68d1434be891ad7b3c1d324c460d05a481f3
David Ernstrom <david@ernstrom.net>
2015-10-29 18:00:46 -0600

    Disabling some admin pages stuff for now.

--- web__django/urls.py
+++ web__django/urls.py
@@ -2,10 +2,10 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import include, url
-from django.contrib import admin
-from django.contrib.auth.views import logout, login
+#from django.contrib import admin
+#from django.contrib.auth.views import logout, login
 from django.contrib.staticfiles.views import serve
-admin.autodiscover()
+#admin.autodiscover()
 
 from webpages._SiteWide.views import load_dutils_js
 from webpages.kiosk.views import home
@@ -13,11 +13,11 @@ from webpages.kiosk.views import home
 
 urlpatterns = [
     # These are Django canned pages
-    url(r'^accounts/logout/$', logout, {'next_page': '/'},
-        name='logout'),
-    url(r'^accounts/login/$', login, {'template_name': 'admin/login.html'}),
-    url(r'^logout/$', logout, {'next_page': '/'}),
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^accounts/logout/$', logout, {'next_page': '/'},
+    #    name='logout'),
+    #url(r'^accounts/login/$', login, {'template_name': 'admin/login.html'}),
+    #url(r'^logout/$', logout, {'next_page': '/'}),
+    #url(r'^admin/', include(admin.site.urls)),
 
     # This is the frowned-upon way of serving static files in Django
     url(r'^static/(?P<path>.*)$', serve, {'insecure': True}),
